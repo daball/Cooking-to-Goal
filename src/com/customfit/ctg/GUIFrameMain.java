3e4b39575dae8aa266f3830f90b86946b629a175
sdsalyer <spencer.salyer@gmail.com>
2011-04-14 19:30:19 -0400

    Prepared main GUI for modular panels

--- src/com/customfit/ctg/GUIFrameMain.java
+++ src/com/customfit/ctg/GUIFrameMain.java
@@ -1,5 +1,8 @@
 package com.customfit.ctg;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -11,7 +14,7 @@ import javax.swing.JPanel;
 public class GUIFrameMain extends JFrame{
 	
 	private static final long serialVersionUID = 1L;
-	private static final String APPLICATION_TITLE = "Cooking to Goal";
+	private static final String APPLICATION_TITLE = "Cooking to Goal 1.0";
 	private static final int WINDOW_WIDTH = 800,
 							 WINDOW_HEIGHT = 600,
 							 WINDOW_X = 100,
@@ -23,7 +26,12 @@ public class GUIFrameMain extends JFrame{
 	public GUIFrameMain(){
 		JPanel panel = new JPanel();
 		
-		panel.add(new JLabel("Hello World!"), JLabel.CENTER);
+//		panel.add(new JLabel("Hello World!"), JLabel.CENTER);
+		
+		panel.setLayout(new BorderLayout());
+		panel.add(titlePanel(), BorderLayout.NORTH);
+		panel.add(centerPanel(), BorderLayout.CENTER);
+//		panel.add(new ButtonPanel());
 		
 		this.add(panel);
 		
@@ -31,5 +39,21 @@ public class GUIFrameMain extends JFrame{
 		this.setLocation(WINDOW_X, WINDOW_Y);
 		this.setTitle(APPLICATION_TITLE);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	private JPanel titlePanel(){
+		JPanel panel = new JPanel();
+		panel.add(new JLabel(APPLICATION_TITLE, JLabel.CENTER));
+		return panel;
+	}
+	
+	private JPanel centerPanel(){
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(1, 2));
+		
+//		panel.add(new GUIMainMenuRecipes());
+//		panel.add(new GUIMainMenuMenuList());
+		
+		return panel;
 	}
 }
