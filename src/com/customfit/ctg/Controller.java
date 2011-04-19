a9e8ead83954be057a16062006efebb75840abfd
David Ball <whatspaz@gmail.com>
2011-04-19 06:27:03 -0400

    i tried to fix code tabbing (visual issue)

--- src/com/customfit/ctg/Controller.java
+++ src/com/customfit/ctg/Controller.java
@@ -7,42 +7,41 @@ import javax.swing.UnsupportedLookAndFeelException;
 /**
  * The Controller class for the 
  * Cooking to Goal application.
- * @author Drew
+ * @author Drew, David
  */
 public class Controller {
-	
-	/**
-	 * The main entry point for the 
-	 * Cooking to Goal application.
-	 * @param args
-	 */
-	public static void main(String args[]) {
-		//make this look more like Windows, Linux, or whatever
-		//besides that junky metal look
+
+    /**
+     * The main entry point for the 
+     * Cooking to Goal application.
+     * @param args
+     */
+    public static void main(String args[]) {
+        //make this look more like Windows, Linux, or whatever
+        //besides that junky metal look
         try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (UnsupportedLookAndFeelException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		//create main window
-        GUIFrameMain gui = new GUIFrameMain();
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         
+        //create main window
+        GUIFrameMain gui = new GUIFrameMain();
+
         //display main window
         gui.setVisible(true);
-        
+
         //I recommend all GUI designers switch to NetBeans
         //for extremely easy coding. -David
-	}
-
+    }
 }
